fa00448e8b3b87b420b2b83a8a9656540ab8e061
zkobrinsky <zachkobrinsky@gmail.com>
2021-05-18 15:45:58 -0400

    added css to disable pulldown reload on mobile

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import React from 'react';
-import './App.css';
-import SandboxSketch from './components/SandboxSketch'
-import Sketches from './components/Sketches'
-import CreateSketch from './components/CreateSketch'
-import SketchShow from './components/SketchShow'
-import MyNav from './components/MyNav'
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import { connect } from 'react-redux';
-import { getSketches } from './redux/actions/sketchActions';
-
-
-
+import React from "react";
+import "./App.css";
+import SandboxSketch from "./components/SandboxSketch";
+import Sketches from "./components/Sketches";
+import CreateSketch from "./components/CreateSketch";
+import SketchShow from "./components/SketchShow";
+import MyNav from "./components/MyNav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import { getSketches } from "./redux/actions/sketchActions";
 
 class App extends React.Component {
-
   componentDidMount() {
-    this.props.getSketches()
+    this.props.getSketches();
   }
 
-  render () {
+  render() {
     return (
-    <div className="App">
-      <Router >
-      <MyNav />
-        <Switch>
-          <Route path="/sketches/new">{(routerProps) => <CreateSketch {...routerProps}/>}</Route>
-          <Route path="/sketches/:id">{(routerProps) => <SketchShow {...routerProps}/>}</Route>
-          <Route path="/sketches"><Sketches/></Route> 
-          <Route path="/">
-            {(props) => (
-              <SandboxSketch {...props} angle={0} rotateRate={0.5} />
-            )} 
-          </Route>
-
-        </Switch>
-      </Router>
-    </div>
-    )
+      <div className="App">
+        <Router>
+          <MyNav />
+          <Switch>
+            <Route path="/sketches/new">
+              {routerProps => <CreateSketch {...routerProps} />}
+            </Route>
+            <Route path="/sketches/:id">
+              {routerProps => <SketchShow {...routerProps} />}
+            </Route>
+            <Route path="/sketches">
+              <Sketches />
+            </Route>
+            <Route path="/">
+              {props => (
+                <SandboxSketch
+                  {...props}
+                  angle={0}
+                  rotateRate={0.5}
+                  className="sandbox"
+                />
+              )}
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
-export default connect(null, { getSketches })(App)
+export default connect(null, { getSketches })(App);
